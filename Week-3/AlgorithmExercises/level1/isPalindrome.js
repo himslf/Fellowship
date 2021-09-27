ac7a4ae31287864ddcef739a3b20422b9978d049
J. J. YOUNG <41083436+himslf@users.noreply.github.com>
2021-09-27 18:38:38 -0400

    Algo. Exers Upload

--- Week-3/AlgorithmExercises/level1/isPalindrome.js
+++ Week-3/AlgorithmExercises/level1/isPalindrome.js
@@ -1,8 +1,9 @@
 /**
  * Determines whether the given string is a palindrome, i.e. if you were to reverse the order of the characters inside the string, you would get back the original string.
  */
-function isPalindrome() {
-
+ function isPalindrome(aString) {
+   const backwardz = aString.split('').reverse().join('');
+   return backwardz == aString;
 }
 
 describe('isPalindrome', function() {
--- Week-3/AlgorithmExercises/level1/reverseString.js
+++ Week-3/AlgorithmExercises/level1/reverseString.js
@@ -1,11 +1,17 @@
 /*
- * Given a string, returns a new string that is the same as the original string with the order of its characters reversed.
- * You're not allowed to use String.prototype.reverse().
- * Returns the new string.
+    Given a string, returns a new string that is the same as the
+    original string with the order of its characters reversed.
+    You're not allowed to use String.prototype.reverse().
+    Returns the new string.
  */
-function reverseString() {
 
-}
+function reverseString(aString) {
+  let returnedString = "";
+  for (let i = aString.length - 1; i >= 0; i--) {
+    returnedString += aString[i];
+  }
+  return returnedString;
+} // end of function
 
 describe('reverseString', function() {
   const assert = require("chai").assert;
